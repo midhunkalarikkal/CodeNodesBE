78a771d9c71ad24e60c538f5c2f813798b7a01bf
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-12-10 23:45:56 +0530

    signup sample route created for post

--- src/app.js
+++ src/app.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const connectDB = require('./config/databse');
+const connectDB = require('./config/database');
+const User = require('./model/user');
 
 const app = express()
 
+app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+app.post('/signup',async(req,res) => {
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("User saved successfully.");
+    }catch(err){
+        res.send("Error saving user in to database.");
+    }
+})
+
+connectDB().then(() => {
+    console.log("Databse connection established.");
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000");
+    });
+}).catch((err) => {
+    console.log("Databse connecting error.");
+})
+
+
 
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-
 const connectDB = async () => {
         await mongoose.connect("mongodb+srv://midhunkpaniker:acpaVOaNX2Hje1i8@cluster0.bvv3j.mongodb.net/codeNodes")
 }
 
-module.exports = { connectDB }
+module.exports = connectDB;
